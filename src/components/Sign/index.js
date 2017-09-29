16aa3af4a65897c15691a5799b70e6a556c4ca39
Keri Spencer <kerigspencer@gmail.com>
2017-09-29 15:24:27 -0400

    Work on bugs

--- src/components/Sign/index.js
+++ src/components/Sign/index.js
@@ -1,32 +1,38 @@
-import React, { Component } from 'react';
-import './index.css';
+import React, { Component } from 'react'
+import './index.css'
 
 class Sign extends Component {
   render() {
     return (
-      <div className='sign'>
-        <form >
+      <div className="sign">
+        <form>
           <div class="imgcontainer">
             <img src="" alt="" class="" />
           </div>
 
           <div class="container">
-            <label><b>Username</b></label>
+            <label>
+              <b>Username</b>
+            </label>
             <input type="text" placeholder="Enter Username" name="uname" required />
-
-            <label><b>Password</b></label>
+            <label>
+              <b>Password</b>
+            </label>
             <input type="password" placeholder="Enter Password" name="psw" required />
-
             <button type="submit">Login</button>
-            <input type="checkbox" checked="checked"/> Remember me
+            <input type="checkbox" checked="checked" /> Remember me
           </div>
 
-          <div class="container" style="background-color:#f1f1f1">
-            <button type="button" class="cancelbtn">Cancel</button>
-            <span class="psw">Forgot <a href="#">password?</a></span>
+          <div class="container">
+            <button type="button" class="cancelbtn">
+              Cancel
+            </button>
+            <span class="psw">
+              Forgot <a href="#">password?</a>
+            </span>
           </div>
         </form>
-    </div>
+      </div>
     )
   }
 }
